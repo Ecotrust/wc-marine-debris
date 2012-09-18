8fede16b992e0002af56ed03b105689e5a96196a
Ryan Hodges <rhodges@ecotrust.org>
2012-09-18 11:11:14 -0700

    map-clicks now update lat/lon fields

--- marine_debris/media/js/widgets/map.js
+++ marine_debris/media/js/widgets/map.js
@@ -1,5 +1,38 @@
 var layerExtent = new OpenLayers.Bounds( -14050000, 3800000, -13000000 , 6280000);
+
 var map = new OpenLayers.Map("map", {'restrictedExtent': layerExtent});
+
+OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {
+    defaultHandlerOptions: {
+        'single': true,
+        'double': false,
+        'pixelTolerance': 0,
+        'stopSingle': false,
+        'stopDouble': false
+    },
+    initialize: function(options) {
+        this.handlerOptions = OpenLayers.Util.extend(
+            {}, this.defaultHandlerOptions
+        );
+        OpenLayers.Control.prototype.initialize.apply(
+            this, arguments
+        );
+        this.handler = new OpenLayers.Handler.Click(
+            this, {
+                'click': this.trigger
+            }, this.handlerOptions
+        );
+    },
+    trigger: function(e) {
+        var lonlat = map.getLonLatFromPixel(e.xy).transform(new OpenLayers.Projection("EPSG:900913"), new OpenLayers.Projection("EPSG:4326"));
+        try{
+            updateCoordVals(lonlat.lon, lonlat.lat);
+        }
+        catch(e){
+            // function updateCoordVals(lon, lat) not available
+        }
+    }
+}); 
 
 var map_extent = new OpenLayers.Bounds(-20037508, -20037508, 20037508, 20037508.34);	
     
@@ -19,6 +52,7 @@ var map_options = {
 
 esriOcean = new OpenLayers.Layer.XYZ("ESRI Ocean", "http://services.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/${z}/${y}/${x}", {
     sphericalMercator: true,
+    projection: map_options.displayProjection,
     isBaseLayer: true,
     // numZoomLevels: 13,
     maxExtent: map_extent,
@@ -26,3 +60,6 @@ esriOcean = new OpenLayers.Layer.XYZ("ESRI Ocean", "http://services.arcgisonline
 
 map.addLayers([esriOcean]);
 map.zoomToExtent(map.restrictedExtent);
+var click = new OpenLayers.Control.Click();
+map.addControl(click);
+click.activate(); 
