013c8253be55c2ed4321204e281ec110e073e3eb
Ryan Hodges <rhodges@ecotrust.org>
2012-09-19 11:45:32 -0700

    Adding points to map selection

--- marine_debris/media/js/widgets/map.js
+++ marine_debris/media/js/widgets/map.js
@@ -2,38 +2,6 @@ var layerExtent = new OpenLayers.Bounds( -14050000, 3800000, -13000000 , 6280000
 
 var map = new OpenLayers.Map("map", {'restrictedExtent': layerExtent});
 
-OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {
-    defaultHandlerOptions: {
-        'single': true,
-        'double': false,
-        'pixelTolerance': 0,
-        'stopSingle': false,
-        'stopDouble': false
-    },
-    initialize: function(options) {
-        this.handlerOptions = OpenLayers.Util.extend(
-            {}, this.defaultHandlerOptions
-        );
-        OpenLayers.Control.prototype.initialize.apply(
-            this, arguments
-        );
-        this.handler = new OpenLayers.Handler.Click(
-            this, {
-                'click': this.trigger
-            }, this.handlerOptions
-        );
-    },
-    trigger: function(e) {
-        var lonlat = map.getLonLatFromPixel(e.xy).transform(new OpenLayers.Projection("EPSG:900913"), new OpenLayers.Projection("EPSG:4326"));
-        try{
-            updateCoordVals(lonlat.lon, lonlat.lat);
-        }
-        catch(e){
-            // function updateCoordVals(lon, lat) not available
-        }
-    }
-}); 
-
 var map_extent = new OpenLayers.Bounds(-20037508, -20037508, 20037508, 20037508.34);	
     
 //Map base options
@@ -58,8 +26,33 @@ esriOcean = new OpenLayers.Layer.XYZ("ESRI Ocean", "http://services.arcgisonline
     maxExtent: map_extent,
 });
 
-map.addLayers([esriOcean]);
+var pointLayer = new OpenLayers.Layer.Vector("Point Layer");
+
+var point = new OpenLayers.Control.DrawFeature(
+    pointLayer,
+    OpenLayers.Handler.Point,
+    {
+        "featureAdded": pointDrawn
+    }
+);
+
+function pointDrawn(point) {
+    var lonlat = point.geometry.transform(new OpenLayers.Projection("EPSG:900913"), new OpenLayers.Projection("EPSG:4326"));
+    clearOldPoints(point);
+    updateCoordVals(lonlat.x, lonlat.y);
+}
+
+// Only display the newest selected point
+function clearOldPoints(point){
+    for (var i = 0; i <= pointLayer.features.length; i++){
+        if (pointLayer.features[i] != point){
+            pointLayer.removeFeatures(pointLayer.features[i]);
+        }
+    }
+}
+
+map.addLayers([esriOcean, pointLayer]);
 map.zoomToExtent(map.restrictedExtent);
-var click = new OpenLayers.Control.Click();
-map.addControl(click);
-click.activate(); 
+
+map.addControl(point);
+point.activate();
