0339e3c9118acfc1418bcc73b3e9aed14d9f649b
Ryan Hodges <rhodges@ecotrust.org>
2012-09-27 17:57:31 -0700

    clean up error handling and data management for create event pages

--- marine_debris/core/views.py
+++ marine_debris/core/views.py
@@ -72,22 +72,27 @@ def create_event(request):
             event['data_sheet'] = form.data['data_sheet']
             
             datasheet = DataSheet.objects.get(sheetname=event['data_sheet'])
-            sites = []
-            sitenames = []
             if datasheet.type_id and not datasheet.type_id.display_sites:
                 form.fields['site_name'].widget = form.fields['site_name'].hidden_widget()
                 form.fields['county'].widget = form.fields['county'].hidden_widget()
-            else:
-                for site in Site.objects.all().exclude(sitename=''):
-                    sites.append({'name':site.sitename, 'lat':site.lat, 'lon':site.lon, 'state':site.state, 'county':site.county})
-                    sitenames.append(str(site.sitename))
-                    
+            
             state_dict = [state.toDict for state in State.objects.all()]
             state_json = simplejson.dumps(state_dict)
             
-            return render_to_response('event_location.html', RequestContext(request, {'form':form.as_p(), 'states': state_json, 'event': event, 'sites':sites, 'sitenames': sitenames, 'active':'events'}))
+            return render_to_response('event_location.html', RequestContext(request, {'form':form.as_p(), 'states': state_json, 'event': event, 'active':'events'}))
         else:
-            return render_to_response('create_event.html', RequestContext(request,{'form':form.as_p(), 'error':'Form is not valid, please review.', 'active':'events'}))
+        
+            form.fields['state'].widget = form.fields['state'].hidden_widget()
+            form.fields['county'].widget = form.fields['county'].hidden_widget()
+            form.fields['site_name'].widget = form.fields['site_name'].hidden_widget()
+            form.fields['latitude'].widget = form.fields['latitude'].hidden_widget()
+            form.fields['longitude'].widget = form.fields['longitude'].hidden_widget()
+
+            #TODO: Filter Organizations by only those which the user has access to.
+            org_dict = [org.toDict for org in Organization.objects.all()]
+            org_json = simplejson.dumps(org_dict)
+        
+            return render_to_response('create_event.html', RequestContext(request,{'form':form.as_p(), 'json':org_json, 'error':'Form is not valid, please review.', 'active':'events'}))
 
 @login_required            
 def event_location(request):
@@ -120,7 +125,11 @@ def event_location(request):
         eventForm.fields['project'].widget = eventForm.fields['project'].hidden_widget()
         eventForm.fields['date'].widget = eventForm.fields['date'].hidden_widget()
         eventForm.fields['data_sheet'].widget = eventForm.fields['data_sheet'].hidden_widget()
-        return render_to_response('event_location.html', RequestContext(request, {'form': eventForm.as_p(), 'event':event, 'active': 'events', 'error': 'There is an error in your location data. Please be sure to fill out all required fields.'}))
+        
+        state_dict = [state.toDict for state in State.objects.all()]
+        state_json = simplejson.dumps(state_dict)
+        
+        return render_to_response('event_location.html', RequestContext(request, {'form': eventForm.as_p(), 'states': state_json, 'event':event, 'active': 'events', 'error': 'There is an error in your location data. Please be sure to fill out all required fields.'}))
 
 @login_required
 def event_save(request):
