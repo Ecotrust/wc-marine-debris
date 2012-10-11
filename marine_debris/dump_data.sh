eb6902f0f8179856035ce0b00850bdb97ce34890
Ryan Hodges <rhodges@ecotrust.org>
2012-10-11 10:43:48 -0700

    updating fixtures and dump for both with and without event data.

--- marine_debris/dump_data.sh
+++ marine_debris/dump_data.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python manage.py dumpdata --indent=2 auth.User core > core/fixtures/dump_data.json
+python manage.py dumpdata --indent=2 auth.User core.datatype core.unit core.organization core.grouping core.field core.eventtype core.datasheet core.datasheetfield core.project core.projectorganization core.media core.projectdatasheet core.state core.county core.site > core/fixtures/dump_data.json
+python manage.py dumpdata --indent=2 auth.User core > core/fixtures/dump_data_w_events.json
