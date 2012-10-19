68c7a47e92f8f51c292a42f37f3d785fabb21f95
Ryan Hodges <rhodges@ecotrust.org>
2012-10-19 11:37:34 -0700

    passing tie-ins for events page to alert user of added event

--- marine_debris/core/views.py
+++ marine_debris/core/views.py
@@ -48,8 +48,11 @@ def index(request):
 
 def events(request, submit=False): 
 
-    return render_to_response( 'events.html', RequestContext(request,{'submit':submit, 'active':'events'}))
-    
+    if submit:
+        return render_to_response( 'events.html', RequestContext(request,{'submit':True, 'added_site':submit, 'active':'events'}))
+    else:
+        return render_to_response( 'events.html', RequestContext(request,{'submit':submit, 'added_site':None, 'active':'events'}))
+        
 def get_locations(request):
     states = []
     locations = {}
@@ -336,7 +339,7 @@ def event_save(request):
             datasheetForm = DataSheetForm(event.datasheet_id, event, None, request.POST)
         if event.id and datasheetForm.is_valid():
             datasheetForm.save()
-            return HttpResponseRedirect('/events/True')
+            return HttpResponseRedirect('/events/%s' % event.id)
         else:
             Event.delete(event)
             if site[1]:
