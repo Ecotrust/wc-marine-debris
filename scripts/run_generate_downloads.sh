c2789e0435c01ea947a3acae12a6274d14415935
Ryan Hodges <rhodges@ecotrust.org>
2012-12-07 18:46:51 -0800

    updating scripts

--- scripts/run_generate_downloads.sh
+++ scripts/run_generate_downloads.sh
@@ -4,5 +4,5 @@
 # sudo crontab -e
 # 0 3 * * * sh /usr/local/apps/wc-marine-debris/scripts/run_generate_downloads.sh
 
-. /usr/local/apps/wc-marine-debris/env/bin/activate
-python /usr/local/apps/wc-marine-debris/marine_debris/manage.py generate_downloads clear
+. /usr/local/apps/WCGA/wc-marine-debris/env/bin/activate
+python /usr/local/apps/WCGA/wc-marine-debris/marine_debris/manage.py generate_downloads clear
