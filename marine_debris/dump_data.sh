70673b35e5d589c444df09806443956cf10d1e16
Ryan Hodges <rhodges@ecotrust.org>
2012-12-19 14:36:56 -0800

    updating fixtures to include flat-blocks and categories.

--- marine_debris/dump_data.sh
+++ marine_debris/dump_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python manage.py dumpdata --indent=2 auth.User core.datatype core.unit core.organization core.grouping core.answeroption core.field core.eventtype core.datasheet core.datasheetfield core.project core.projectorganization core.media core.projectdatasheet core.state core.county core.site core.usertransaction > core/fixtures/dump_data.json
+python manage.py dumpdata --indent=2 auth.User core.datatype core.unit core.unitconversion core.download core.organization core.grouping core.answeroption core.displaycategory core.field core.eventtype core.datasheet core.datasheetfield core.project core.projectorganization core.media core.projectdatasheet core.state core.site core.county > core/fixtures/dump_data.json
 python manage.py dumpdata --indent=2 auth.User core > core/fixtures/dump_data_w_events.json
