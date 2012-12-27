afc42b928c11d12deb48436b234b6a4ff07b96b5
Ryan Hodges <rhodges@ecotrust.org>
2012-12-26 19:41:10 -0800

    updating dump data script to gzip

--- marine_debris/dump_data.sh
+++ marine_debris/dump_data.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 python manage.py dumpdata --indent=2 auth.User flatblocks.flatblock core.datatype core.unit core.unitconversion core.download core.organization core.grouping core.answeroption core.displaycategory core.field core.eventtype core.datasheet core.datasheetfield core.project core.projectorganization core.media core.projectdatasheet core.state core.usertransaction core.site core.county > core/fixtures/dump_data.json
+rm  core/fixtures/dump_data.json.gz
+gzip  core/fixtures/dump_data.json
 python manage.py dumpdata --indent=2 flatblocks.flatblock core.datatype core.unit core.unitconversion core.download core.organization core.grouping core.answeroption core.displaycategory core.field core.eventtype core.datasheet core.datasheetfield core.project core.projectorganization core.media core.projectdatasheet core.state core.site core.usertransaction core.county > core/fixtures/update_data.json
+rm core/fixtures/update_data.json.gz
+gzip core/fixtures/update_data.json
 python manage.py dumpdata --indent=2 auth.User flatblocks.flatblock core > core/fixtures/dump_data_w_events.json
+rm core/fixtures/dump_data_w_events.json.gz
+gzip core/fixtures/dump_data_w_events.json
