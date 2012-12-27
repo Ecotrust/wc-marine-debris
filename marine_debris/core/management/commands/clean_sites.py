79cf7202b868a177fa9f17a8879499a0217b550d
Ryan Hodges <rhodges@ecotrust.org>
2012-12-26 18:29:21 -0800

    updating clean_sites to get actual duplicates, too

--- marine_debris/core/management/commands/clean_sites.py
+++ marine_debris/core/management/commands/clean_sites.py
@@ -30,4 +30,16 @@ class Command(BaseCommand):
                     site.sitename = site.sitename.strip()
                     site.county = site.county.strip()
                     site.save()
-        
+            else:
+                dupes = Site.objects.filter(sitename = site.sitename, county = site.county)
+                if dupes.count() > 1:
+                    master_site = dupes.order_by('id')[0]
+                    for dupe in dupes:
+                        if not dupe.id == master_site.id:
+                            events = Event.objects.filter(site = dupe)
+                            for event in events:
+                                print 'changing event ' + str(event.id) + '\'s site from ' + str(event.site.id) + ' to ' + str(master_site.id)
+                                event.site = master_site
+                                event.save()
+                            print 'Deleting dupe site ' + dupe.sitename + ' id: ' + dupe.id
+                            dupe.delete()
