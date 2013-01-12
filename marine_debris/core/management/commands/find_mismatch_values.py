deaefa05ecfa0dace9b590834573a369be52b744
Ryan Hodges <rhodges@ecotrust.org>
2013-01-11 19:46:24 -0800

    making the find_mismatch_values command MUCH faster

--- marine_debris/core/management/commands/find_mismatch_values.py
+++ marine_debris/core/management/commands/find_mismatch_values.py
@@ -8,17 +8,22 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         count = 0
         mismatches = {}
-        for fv in FieldValue.objects.all():
-            dsf = fv.event_id.datasheet_id.datasheetfield_set.get(field_id=fv.field_id)
-            key = 'dsf:%s-f:%s' % (str(dsf.id), str(fv.field_id.id))
-            if not mismatches.has_key(key):
-                if dsf.unit_id and fv.field_id.unit_id and not dsf.unit_id.data_type == fv.field_id.unit_id.data_type:
-                    mismatches[key] = {
-                        'datasheet' : dsf.sheet_id,
-                        'datasheetfield': dsf,
-                        'field': fv.field_id
-                    }
+        
+        for ds in DataSheet.objects.all():
+            for dsf in ds.datasheetfield_set.all():
+                key = 'dsf:%s' % str(dsf.id)
+                if not mismatches.has_key(key):
+                    if dsf.unit_id and dsf.field_id.unit_id and not dsf.unit_id.data_type == dsf.field_id.unit_id.data_type:
+                        mismatches[key] = {
+                            'datasheet' : ds,
+                            'datasheetfield': dsf,
+                            'field': dsf.field_id
+                        }
+
         for key in mismatches:
             mismatch =  mismatches[key]
-            print 'Value mismatch at %s: dsf %s has value %s; field has value %s' % (mismatch['datasheet'].sheetname, mismatch['datasheetfield'].field_name, mismatch['datasheetfield'].unit_id.short_name, mismatch['field'].unit_id.short_name)
+            print 'Value mismatch at %s %s: \n\
+            dsf %s has value %s \n\
+            field has value %s\n\
+            ' % (mismatch['datasheet'].created_by.orgname, mismatch['datasheet'].sheetname, mismatch['datasheetfield'].field_name, mismatch['datasheetfield'].unit_id.short_name, mismatch['field'].unit_id.short_name)
                 
