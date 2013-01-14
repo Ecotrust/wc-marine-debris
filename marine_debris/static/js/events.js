ff17a4af53a217c6c3bd201328cbbee4439e3eee
Ryan Hodges <rhodges@ecotrust.org>
2013-01-14 14:21:56 -0800

    using new API key

--- marine_debris/static/js/events.js
+++ marine_debris/static/js/events.js
@@ -633,7 +633,7 @@ app.initMap = function () {
 
   var hybrid = new OpenLayers.Layer.Bing({
                 name: "Hybrid",
-                key: 'AiEF9gzhYiOfxnplJmKcY768T9GhG071ww0DizfaPFi7AnAKpBAJQ_UrHadSgDX4',
+                key: 'AvD-cuulbvBqwFDQGNB1gCXDEH4S6sEkS7Yw9r79gOyCvd2hBvQYPaRBem8cpkjv',
                 type: "AerialWithLabels"
             });
 
