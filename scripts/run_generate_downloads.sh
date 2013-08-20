8a66cbf5543ff3df745f115a2568b18c29f20ca2
Ryan Hodges <rhodges@ecotrust.org>
2013-08-20 11:40:40 -0700

    adding logging to download build job

--- scripts/run_generate_downloads.sh
+++ scripts/run_generate_downloads.sh
@@ -4,5 +4,11 @@
 # sudo crontab -e
 # 0 3 * * * sh /usr/local/apps/wc-marine-debris/scripts/run_generate_downloads.sh
 
+echo "$(date) -- " >> /usr/local/apps/WCGA/wc-marine-debris/logs/downloads.log
 . /usr/local/apps/WCGA/wc-marine-debris/env/bin/activate
-python /usr/local/apps/WCGA/wc-marine-debris/marine_debris/manage.py generate_downloads clear
+python /usr/local/apps/WCGA/wc-marine-debris/marine_debris/manage.py generate_downloads clear >> /usr/local/apps/WCGA/wc-marine-debris/logs/downloads.log
+status=$?
+if [ $status -ne "0" ]; then
+    echo "$(date) -- ERROR: Creating downloads failed with a status code of $status" >> /usr/local/apps/WCGA/wc-marine-debris/logs/downloads.log
+    exit 1
+fi
