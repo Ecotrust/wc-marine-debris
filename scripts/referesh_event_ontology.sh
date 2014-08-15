268bd8fb5af787f5ef3059d6b4241ad921dc6e95
Seth Hill <seth@pointnineseven.com>
2014-08-15 13:43:34 -0700

    Shell script to refresh the ontology view

---
+++ scripts/referesh_event_ontology.sh
@@ -0,0 +1,17 @@
+# Rebuilds the event_ontology table. 
+# Should be run about once per day (as root)
+#
+# SRH 15-Aug-2014
+#
+# cd /etc/cron.daily
+# ln -s /usr/local/apps/wc-marine-debris/marine_debris/refresh_event_ontology.sh
+
+APPDIR=/usr/local/apps/wc-marine_debris
+PYTHON=/usr/local/venv/marine-debris/bin/python
+
+# run the SQL query (takes about 15 seconds as of 15-Aug)
+psql -U marine_debris < $APPDIR/marine_debris/event_ontology.sql
+
+# flush the redis cache
+cd $APPDIR/marine_debris
+$PYTHON manage.py flush_caches
