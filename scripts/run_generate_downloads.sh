7a4b8dff02a7c904473ad1f9118a7f4b8b86193d
Seth Hill <seth@pointnineseven.com>
2015-04-13 11:27:48 -0700

    Script updates

--- scripts/run_generate_downloads.sh
+++ scripts/run_generate_downloads.sh
@@ -4,11 +4,20 @@
 # sudo crontab -e
 # 0 3 * * * sh /usr/local/apps/wc-marine-debris/scripts/run_generate_downloads.sh
 
-echo "$(date) -- " >> /usr/local/apps/WCGA/wc-marine-debris/logs/downloads.log
-. /usr/local/apps/WCGA/wc-marine-debris/env/bin/activate
-python /vagrant/marine_debris/manage.py generate_downloads clear >> /usr/local/apps/WCGA/wc-marine-debris/logs/downloads.log
+APPDIR=/usr/local/apps/wc-marine-debris
+ENV=/usr/local/env/marine_debris
+LOG_DIR=/usr/local/apps/wc-marine-debris/logs
+LOG=$LOG_DIR/downloads.log
+
+source $ENV/bin/activate
+mkdir -p $LOG_DIR
+cd $APPDIR/marine_debris
+
+echo "$(date) -- " >> $LOG
+
+python manage.py generate_downloads clear >> $LOG
 status=$?
 if [ $status -ne "0" ]; then
-    echo "$(date) -- ERROR: Creating downloads failed with a status code of $status" >> /usr/local/apps/WCGA/wc-marine-debris/logs/downloads.log
+    echo "$(date) -- ERROR: Creating downloads failed with a status code of $status" >> $LOG
     exit 1
 fi
